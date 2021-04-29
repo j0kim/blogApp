9be84f68a8f1bbcdf1471acc5fa07a551313f1bc
j0kim <joachim.wat@gmail.com>
2021-04-29 16:22:04 +0300

    Add update and delete buttons. Implement delete fuctionality

--- dist/main.js
+++ dist/main.js
@@ -3,24 +3,39 @@ let postBtn = document.querySelector("#btnPost");
 let postContainer = document.querySelector('#post-container');
 // Create a new div element when the post button is clicked
 const newElement = () => {
-    let newDiv = document.createElement('div');
-    let newP = document.createElement('p');
-    // let newP2 = document.createElement('p');
-    // let newI = document.createElement('i');
-    // newI.classList.add("fas", "fa-trash");
     let userMsg = document.querySelector("#message");
     let postMsg = document.createTextNode(userMsg.value);
+    // Create divs to hold the user's posts
+    const postDiv = document.createElement('div');
+    const textDiv = document.createElement('div');
+    const editDiv = document.createElement('div');
+    let newP = document.createElement('p');
+    postDiv.classList.add("post");
+    textDiv.classList.add("postText");
+    editDiv.classList.add('editSection');
+    // Create update and delete buttons
+    const btnUpdate = document.createElement('button');
+    const btnDelete = document.createElement('button');
+    btnUpdate.innerHTML = '<i class="fas fa-pen"></i>';
+    btnDelete.innerHTML = '<i class="fas fa-trash"></i>';
     newP.appendChild(postMsg);
-    // newP2.appendChild(newI);
-    newDiv.appendChild(newP);
-    // newDiv.appendChild(newP2);
-    newDiv.classList.add("post");
+    textDiv.appendChild(newP);
+    postDiv.appendChild(textDiv);
+    postDiv.appendChild(editDiv);
+    editDiv.appendChild(btnUpdate);
+    editDiv.appendChild(btnDelete);
+    postDiv.appendChild(editDiv);
     if (userMsg.value === "") {
         alert("Please enter something to post!");
     }
     else {
-        postContainer.appendChild(newDiv);
+        postContainer.appendChild(postDiv);
         userMsg.value = "";
     }
+    btnDelete.addEventListener('click', function () {
+        const parent = this.parentElement;
+        const grandParent = parent === null || parent === void 0 ? void 0 : parent.parentElement;
+        grandParent === null || grandParent === void 0 ? void 0 : grandParent.remove();
+    });
 };
 postBtn.addEventListener('click', newElement, false);
--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,47 @@ let postContainer = document.querySelector('#post-container') as HTMLDivElement;
 // Create a new div element when the post button is clicked
 
 const newElement = () => {
-    let newDiv = document.createElement('div');
-    let newP = document.createElement('p');
-    // let newP2 = document.createElement('p');
-    // let newI = document.createElement('i');
-    // newI.classList.add("fas", "fa-trash");
     let userMsg = document.querySelector("#message") as HTMLInputElement;
     let postMsg = document.createTextNode(userMsg.value);
+
+    // Create divs to hold the user's posts
+    const postDiv = document.createElement('div');
+    const textDiv = document.createElement('div');
+    const editDiv = document.createElement('div');
+    let newP = document.createElement('p');
+    postDiv.classList.add("post");
+    textDiv.classList.add("postText");
+    editDiv.classList.add('editSection');
+
+
+    // Create update and delete buttons
+    const btnUpdate = document.createElement('button');
+    const btnDelete = document.createElement('button');
+    btnUpdate.innerHTML = '<i class="fas fa-pen"></i>';
+    btnDelete.innerHTML = '<i class="fas fa-trash"></i>';
+
+
+
     newP.appendChild(postMsg);
-    // newP2.appendChild(newI);
-    newDiv.appendChild(newP);
-    // newDiv.appendChild(newP2);
-    newDiv.classList.add("post");
+    textDiv.appendChild(newP);
+    postDiv.appendChild(textDiv);
+    postDiv.appendChild(editDiv);
+
+    editDiv.appendChild(btnUpdate);
+    editDiv.appendChild(btnDelete);
+    postDiv.appendChild(editDiv);
     if (userMsg.value === "") {
         alert("Please enter something to post!");
     } else {
-        postContainer.appendChild(newDiv);
+        postContainer.appendChild(postDiv);
         userMsg.value = "";
     }
+
+    btnDelete.addEventListener('click', function () {
+        const parent = this.parentElement;
+        const grandParent = parent?.parentElement;
+        grandParent?.remove();
+    });
 };
 
 postBtn.addEventListener('click', newElement, false);
